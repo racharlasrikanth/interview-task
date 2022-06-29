68c1610888bba4430f5bc21248c812c475f2be08
Srikanth Racharla <srikanth@Srikanths-MacBook-Pro.local>
2022-06-29 12:56:42 +0530

    project completed

--- js/app.js
+++ js/app.js
@@ -2,32 +2,12 @@
 
 function init() {
   const hamburger = document.querySelector(".hamburger");
-  const sidebarCloseButton = document.querySelector(".close-button");
   const sidebarContainer = document.querySelector(".sidebar-container");
 
-  const openSidebar = () => {
-    sidebarContainer.classList.add("show-sidebar");
+  const toggleSidebar = () => {
+    sidebarContainer.classList.toggle("show-sidebar");
   };
 
-  const closeSidebar = () => {
-    sidebarContainer.classList.remove("show-sidebar");
-  };
-
-  hamburger.addEventListener("click", openSidebar);
-  sidebarCloseButton.addEventListener("click", closeSidebar);
+  hamburger.addEventListener("click", toggleSidebar);
 }
 init();
-
-function navRelatedCode() {
-  const navbarContainer = document.querySelector(".nav-container");
-  window.addEventListener("scroll", function () {
-    const scrollNumber = window.scrollY;
-    const targetNumber = window.innerWidth >= 992 ? 70 : 50;
-    if (scrollNumber >= targetNumber) {
-      navbarContainer.classList.add("sticky-nav");
-    } else {
-      navbarContainer.classList.remove("sticky-nav");
-    }
-  });
-}
-navRelatedCode();
